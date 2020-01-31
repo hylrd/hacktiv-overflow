69812fd1dda478850847fb279e1648d08191eccf
haekal <haekal.yudh@gmail.com>
2020-01-31 14:17:51 +0700

    test

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:3000',
+	baseURL: 'http://35.198.234.158',
 	timeout: (60 * 1000), // accept integers in milisecond = 1min
 })
 
